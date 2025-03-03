18ff7bc9c4b04e3ac334e04fca486a20c4a3b609
nehakolambe <kolambe.neha36@gmail.com>
2025-03-03 10:11:49 -0700

    setup docusaurus

---
+++ docs/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/nehakolambe/Documents/crypto-dashboard/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/nehakolambe/Documents/crypto-dashboard/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/nehakolambe/Documents/crypto-dashboard/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/nehakolambe/Documents/crypto-dashboard/docs/src/css/custom.css"),
+];
